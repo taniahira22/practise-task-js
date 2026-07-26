7ae363ba6138cd1b5162debb6c717a53dba98adf
taniahira22 <tania.hira2020@gmail.com>
2026-07-26 22:46:37 +0600

    first commit

---
+++ task4.js
@@ -0,0 +1,10 @@
+let a = 100;
+let b ="programing";
+let c = true;
+let d =false;
+let e =3.1416;
+console.log("a:",typeof a);
+console.log("b:",typeof b);
+console.log("c:",typeof c);
+console.log("d:",typeof d);
+console.log("e:",typeof e);
