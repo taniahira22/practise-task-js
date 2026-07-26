475a4044545ab6dca328f3e1baafa28f1ccdf1af
taniahira22 <tania.hira2020@gmail.com>
2026-07-26 22:40:54 +0600

    first commit

---
+++ task3.js
@@ -0,0 +1,8 @@
+let studentName ="Alex Johnson";
+let age = 20;
+let country ="Bangladesh";
+let isStudent = true;
+console.log("Student Name:",studentName);
+console.log("Age:",age);
+console.log("Country:",country);
+console.log("Is Student:",isStudent);
