eac1ada5d50bcd70d7cbe28610cbdbbe4ce0849a
taniahira22 <tania.hira2020@gmail.com>
2026-07-26 23:10:02 +0600

    first commit

---
+++ task6.js
@@ -0,0 +1,13 @@
+let name ="Hello";
+let name ="Alex";
+let age = 20;
+let favoriteFood ="pizza";
+let favoriteLanguage ="javaScript";
+let lovesCoding =true;
+console.log("Hello");
+console.log("My name is "+ name +" . ");
+console.log("I am " + age + "years old.");
+console.log("My favorite food is"+favoriteFood+" .");
+console.log("My favorite programming language is"+favoriteLanguage+" .");
+console.log("I love coding:",lovescoding);
+
